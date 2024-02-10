0f3b0a52f4eb1ee208592da9f28583c2c8b06288
luka <lukatskhvedadze5@gmail.com>
2024-02-10 19:04:04 +0400

    first commit

---
+++ day 1/gakvetili1.py
@@ -0,0 +1,40 @@
+name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობუის მიმნიჭებელი სიმბოლო
+# "luka" არის ცვლადისთვის მნისჰვნელობა
+
+surname = "cxvedadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "luka" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მტელი რიცხვი
+height = 170 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #True ან False
+is_ugly = False    #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიფტული cameLcase
+
+
+# print(name + " " + surname)
+
+# #სტრინგი არის ბრჭყალებშ მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
+
+print(type(13))
+print(type("Luka"))
+print(type("Cxvedadze"))
+print(type(1,68))
+print(type(True))
+
